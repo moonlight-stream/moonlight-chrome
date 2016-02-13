b5b6329c9d4d938518af7a6e6b9044f8e4f318a6
Cameron Gutman <aicommander@gmail.com>
2016-02-13 02:15:33 -0500

    Move MoonlightInstance declaration into a header

--- main.cpp
+++ main.cpp
@@ -1,17 +1,6 @@
-#include "ppapi/cpp/instance.h"
-#include "ppapi/cpp/module.h"
-#include "ppapi/cpp/var.h"
+#include "moonlight.hpp"
 
-#include "nacl_io/nacl_io.h"
-
-class MoonlightInstance : public pp::Instance {
-    public:
-        explicit MoonlightInstance(PP_Instance instance) : pp::Instance(instance) {            
-            // This function MUST be used otherwise sockets don't work (nacl_io_init() doesn't work!)            
-            nacl_io_init_ppapi(pp_instance(), pp::Module::Get()->get_browser_interface());
-        }
-        virtual ~MoonlightInstance() {}
-};
+MoonlightInstance::~MoonlightInstance() {}
 
 class MoonlightModule : public pp::Module {
     public:
@@ -25,7 +14,6 @@ class MoonlightModule : public pp::Module {
 
 namespace pp {
 Module* CreateModule() {
-    
     return new MoonlightModule();
 }
 }  // namespace pp
---
+++ moonlight.hpp
@@ -0,0 +1,15 @@
+#include "ppapi/cpp/instance.h"
+#include "ppapi/cpp/module.h"
+#include "ppapi/cpp/var.h"
+
+#include "nacl_io/nacl_io.h"
+
+class MoonlightInstance : public pp::Instance {
+    public:
+        explicit MoonlightInstance(PP_Instance instance) : pp::Instance(instance) {            
+            // This function MUST be used otherwise sockets don't work (nacl_io_init() doesn't work!)            
+            nacl_io_init_ppapi(pp_instance(), pp::Module::Get()->get_browser_interface());
+        }
+        
+        virtual ~MoonlightInstance();
+};
