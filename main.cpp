3cfeca7d923951483550c4f86e0db32ea6990aaf
Aidan Campbell <aidan.campbell@gmail.com>
2016-02-13 17:27:56 -0500

    refactored HTML button hooks moved status messages to new field

--- main.cpp
+++ main.cpp
@@ -2,11 +2,20 @@
 
 #include <pthread.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "ppapi/cpp/input_event.h"
 
 static char s_Host[256];
 static STREAM_CONFIGURATION s_StreamConfig;
+// you pair to a target
+#define PAIR_DIRECTIVE "pair:"
+// you need to show the apps of a target
+#define SHOW_GAMES_DIRECTIVE "showAppsPushed:"
+// you need to use a certain target to start a certain gameID
+#define START_STREAM_DIRECTIVE "setGFEHostIPField:"
+// No parameters. just request a stop.
+#define STOP_DIRECTIVE "stopPushed"
 
 MoonlightInstance* g_Instance;
 
@@ -62,14 +71,37 @@ void* MoonlightInstance::ConnectionThreadFunc(void* context) {
     return NULL;
 }
 
+// hook from javascript into the CPP code.
 void MoonlightInstance::HandleMessage(const pp::Var& var_message) {
-    // Ignore the message if it is not a string.
     if (!var_message.is_string())
-        return;
- 
-    std::string host = var_message.AsString();
+        return;    // Ignore the message if it is not a string.
+    std::string message = var_message.AsString();
 
-    // Populate the stream configuration
+
+    if(strncmp(message.c_str(), PAIR_DIRECTIVE, strlen(PAIR_DIRECTIVE)) == 0) {
+        handlePair(message);
+    } else if (strncmp(message.c_str(), SHOW_GAMES_DIRECTIVE, strlen(SHOW_GAMES_DIRECTIVE)) == 0) {
+        handleShowGames(message);
+    } else if (strncmp(message.c_str(), START_STREAM_DIRECTIVE, strlen(START_STREAM_DIRECTIVE)) == 0) {
+        handleStartStream(message);
+    } else if (strncmp(message.c_str(), STOP_DIRECTIVE, strlen(STOP_DIRECTIVE)) == 0) {
+        handleStopStream(message);
+    } else {
+        //      :(
+    }
+
+}
+
+void MoonlightInstance::handlePair(std::string pairMessage) {
+    std::string intendedHost = pairMessage.substr(pairMessage.find(PAIR_DIRECTIVE) + strlen(PAIR_DIRECTIVE));
+}
+
+void MoonlightInstance::handleShowGames(std::string showGamesMessage) {
+    std::string host = showGamesMessage.substr(showGamesMessage.find(SHOW_GAMES_DIRECTIVE) + strlen(SHOW_GAMES_DIRECTIVE));
+}
+
+void MoonlightInstance::handleStartStream(std::string startStreamMessage) {
+        // Populate the stream configuration
     LiInitializeStreamConfiguration(&s_StreamConfig);
     s_StreamConfig.width = 1280;
     s_StreamConfig.height = 720;
@@ -79,10 +111,13 @@ void MoonlightInstance::HandleMessage(const pp::Var& var_message) {
     s_StreamConfig.streamingRemotely = 0;
     s_StreamConfig.audioConfiguration = AUDIO_CONFIGURATION_STEREO;
 
-    // Store the host
-    host = host.substr(host.find(":") + 1);
+    // Store the host, which is between two colons
+    std::string host = startStreamMessage.substr(strlen(START_STREAM_DIRECTIVE), startStreamMessage.substr(strlen(START_STREAM_DIRECTIVE)).find(":") + 1);
     strcpy(s_Host, host.c_str());
     
+    // store the gameID to start, which is after the last colon
+    std::string gameID = startStreamMessage.substr(startStreamMessage.find(host) + host.length() + 1); // +1 for the colon delimiter
+
     // Post a status update before we begin
     pp::Var response("Starting connection...");
     PostMessage(response);
@@ -90,6 +125,10 @@ void MoonlightInstance::HandleMessage(const pp::Var& var_message) {
     // Start the worker thread to establish the connection
     pthread_t t;
     pthread_create(&t, NULL, MoonlightInstance::ConnectionThreadFunc, this);
+}
+
+void MoonlightInstance::handleStopStream(std::string stopStreamMessage) {
+
 }
 
 bool MoonlightInstance::Init(uint32_t argc,
