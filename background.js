783b1a579932df5d37ef8bd6cc78f3db876ce4ed
Aidan Campbell <aidan.campbell@gmail.com>
2016-02-13 05:00:29 -0500

    cleaning unused code and fixing indentations

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
-    'bounds': {
-      'width': 400,
-      'height': 500
-    }
-  });
+    chrome.app.window.create('index.html', {
+        'bounds': {
+            'width': 400,
+            'height': 500
+        }
+    });
 });
