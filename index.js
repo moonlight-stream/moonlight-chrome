cd908c3378631f5eaee7479f298f2ce98b450d7b
Aidan Campbell <aidan.campbell@gmail.com>
2016-02-13 05:03:28 -0500

    basic webUI for testing

---
+++ index.js
@@ -0,0 +1,38 @@
+// Called by the common.js module.
+function attachListeners() {
+    document.getElementById('startButton').addEventListener('click', startPushed);
+    document.getElementById('stopButton').addEventListener('click', stopPushed);
+    document.getElementById('GFEHostIPField').addEventListener('change', changeGFEHostIPField);
+}
+
+// Called by the common.js module.
+function moduleDidLoad() {
+    // The module is not hidden by default so we can easily see if the plugin
+    // failed to load.
+    common.hideModule();
+}
+
+function getGFEHostIPField() {
+    return document.getElementById('GFEHostIPField');
+}
+
+function startPushed() {
+    common.naclModule.postMessage('setGFEHostIPField:' + getGFEHostIPField().value);
+    common.naclModule.postMessage('startPushed');
+}
+
+function stopPushed() {
+    common.naclModule.postMessage('stopPushed');
+}
+
+function changeGFEHostIPField() {
+    common.naclModule.postMessage('setGFEHostIPField:' + getGFEHostIPField().value);
+}
+
+// Called by the common.js module.
+function handleMessage(msg) {
+    var logEl = document.getElementById('GFEHostIPField');
+    logEl.value = msg.data;
+}
+
+chrome.app.window.current().fullscreen();
