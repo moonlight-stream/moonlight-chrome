6a963f3e6d2d78eb6e05a60cfaf6c0d09d31bbc2
Aidan Campbell <aidan.campbell@gmail.com>
2016-02-13 15:50:11 -0500

    UI updates. Not done, or hooked up.

--- index.js
+++ index.js
@@ -2,25 +2,29 @@
 function attachListeners() {
     document.getElementById('startButton').addEventListener('click', startPushed);
     document.getElementById('stopButton').addEventListener('click', stopPushed);
+    document.getElementById('pairButton').addEventListener('click', pairPushed);
+    document.getElementById('showAppsButton').addEventListener('click', showAppsPushed);
 }
 
-function getGFEHostIPField() {
-    return document.getElementById('GFEHostIPField');
+function pairPushed() {
+    common.naclModule.postMessage('pair:' + document.getElementById('GFEHostIPField').value);
+}
+
+function showAppsPushed() {
+    common.naclModule.postMessage('showAppsPushed');
+    document.getElementById("gameSelectionDiv").style.display = "visible";
 }
 
 function startPushed() {
-    common.naclModule.postMessage('setGFEHostIPField:' + getGFEHostIPField().value);
-    //common.naclModule.postMessage('startPushed');
+    common.naclModule.postMessage('setGFEHostIPField:' + document.getElementById('GFEHostIPField').value);
 }
 
 function stopPushed() {
     common.naclModule.postMessage('stopPushed');
 }
 
-// Called by the common.js module.
+// hook from main.cpp into the javascript
 function handleMessage(msg) {
     var logEl = document.getElementById('GFEHostIPField');
     logEl.value = msg.data;
 }
-
-// chrome.app.window.current().fullscreen();
