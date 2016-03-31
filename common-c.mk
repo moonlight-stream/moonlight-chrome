6ca202581d765a14942c30d6b1144a8059c08e46
Cameron Gutman <aicommander@gmail.com>
2016-03-31 11:54:59 -0400

    Update common-c submodule to stream from Gen 7

--- common-c.mk
+++ common-c.mk
@@ -1,4 +1,4 @@
-COMMON_C_DIR := moonlight-common-c/limelight-common
+COMMON_C_DIR := moonlight-common-c/src
 OPENAES_DIR := $(COMMON_C_DIR)/OpenAES
 ENET_DIR := moonlight-common-c/enet
 
@@ -14,12 +14,6 @@ ENET_SOURCE := \
     $(ENET_DIR)/win32.c     \
 
 ENET_INCLUDE := $(ENET_DIR)/include
-
-OPENAES_SOURCE := \
-	$(OPENAES_DIR)/oaes_base64.c \
-	$(OPENAES_DIR)/oaes_lib.c \
-	
-OPENAES_INCLUDE := $(OPENAES_DIR)
 
 COMMON_C_SOURCE := \
 	$(COMMON_C_DIR)/AudioStream.c         \
@@ -38,9 +32,8 @@ COMMON_C_SOURCE := \
 	$(COMMON_C_DIR)/SdpGenerator.c        \
 	$(COMMON_C_DIR)/VideoDepacketizer.c   \
 	$(COMMON_C_DIR)/VideoStream.c         \
-	$(OPENAES_SOURCE)                     \
     $(ENET_SOURCE)                        \
 
-COMMON_C_INCLUDE := $(COMMON_C_DIR) $(OPENAES_INCLUDE) $(ENET_INCLUDE)
+COMMON_C_INCLUDE := $(COMMON_C_DIR) $(ENET_INCLUDE)
 
 COMMON_C_C_FLAGS := -DLC_CHROME -Wno-missing-braces -DHAS_SOCKLEN_T=1 -DHAS_FCNTL=1 -DNO_MSGAPI=1
