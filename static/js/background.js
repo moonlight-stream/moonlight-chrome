7030576f80e08fd8680d67b1108491afa37c415e
Cameron Gutman <aicommander@gmail.com>
2016-05-06 13:36:10 -0400

    Make the default window size a little bigger

--- static/js/background.js
+++ static/js/background.js
@@ -1,9 +1,8 @@
-// just start the app in fullscreen
 chrome.app.runtime.onLaunched.addListener(function() {
     chrome.app.window.create('index.html', {
         state: "normal",
         bounds: { 
-    		width: 770, height: 440
+    		width: 850, height: 500
     	}
     });
 });
