bd881aaee4d00173a682c9a37deee208ff857b1c
Cameron Gutman <aicommander@gmail.com>
2016-09-24 21:36:38 -0700

    Improve handling of attempting to quit another client's session

--- static/js/utils.js
+++ static/js/utils.js
@@ -438,9 +438,11 @@ NvHTTP.prototype = {
     },
     
     quitApp: function () {
-        return sendMessage('openUrl', [this._baseUrlHttps + '/cancel?' + this._buildUidStr(), false]).then(function () {
-            this.currentGame = 0;
-        }.bind(this));
+        return sendMessage('openUrl', [this._baseUrlHttps + '/cancel?' + this._buildUidStr(), false])
+            // Refresh server info after quitting because it may silently fail if the
+            // session belongs to a different client.
+            // TODO: We should probably bubble this up to our caller.
+            .then(refreshServerInfo().bind(this));
     },
     
     pair: function(randomNumber) {
